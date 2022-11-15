c734d9c4759c608fcc65c6bfb1f3479ea6c4cce3
imvab <vaibhavbirlavb@gmail.com>
2022-11-15 21:00:50 +0530

    Misc

--- Arrays/countNegatives.java
+++ Arrays/countNegatives.java
@@ -1,28 +1,19 @@
 class Solution {
     public int countNegatives(int[][] grid) {
-        int ans = 0;
-        int start = 0;
-        // we will be initializing start=0 as we are comparing from left top corner of
-        // the matrix
+        int count = 0;
+        int row = 0;
+        int col = grid[0].length - 1;
 
-        int end = grid[0].length - 1;
-        // end is refering to right top corner of the matrix
-
-        while (start < grid.length && end >= 0) {
-            if (grid[start][end] < 0) {
-                // if the top left element is negative then we simply add the whole column to
-                // the answer and decrease the column by 1 and again check
-                ans += grid.length - start;
-                end--;
+        while (row < grid.length && col >= 0) {
+            if (grid[row][col] < 0) {
+                count += grid.length - row;
+                col--;
             } else {
-                start++;
-                // if element is positive then we simply increment the row as it is given the
-                // row is sorted which means if we found a positive number then all the numbers
-                // left to it will be positive
+                row++;
             }
-
         }
-        return ans;
-
+        return count;
     }
 }
+
+// https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
