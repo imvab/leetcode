db7feb487e9bec38bac31f134730d53720c1c64e
imvab <vaibhavbirlavb@gmail.com>
2022-11-22 21:10:22 +0530

    Added More

--- String/largestNumber.java
+++ String/largestNumber.java
@@ -26,3 +26,26 @@ class Solution {
         return largestNumberStr;
     }
 }
+
+
+class Solution {
+    public String largestNumber(int[] nums) {
+        ArrayList<String> strings = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            strings.add(String.valueOf(nums[i]));
+        }
+        
+        Collections.sort(strings, (a, b) -> ((a+b).compareTo(b+a) > 0) ? 2 : -2);
+        
+        if (strings.get(0).equals("0")) return "0";
+        
+        String res = "";
+        
+        for (String s : strings) {
+            res += s;
+        }
+        return res;
+    }
+}
+
+// https://leetcode.com/problems/largest-number/
