5aa3fd8dda4a54240d5f57805dec08b454b2aeca
imvab <vaibhavbirlavb@gmail.com>
2023-01-22 11:59:53 +0530

    Contest

--- Sapling/contest.java
+++ Sapling/contest.java
@@ -1,102 +1,96 @@
-import java.util.Arrays;
-import java.util.Scanner;
+class Solution {
+    static ArrayList<Integer> leaders(int arr[], int n) {
+        // Write your code here
+        ArrayList<Integer> array = new ArrayList<Integer>();
+        int current_leader = arr[n - 1];
+        array.add(arr[n - 1]);
+        for (int i = n - 2; i >= 0; i--) {
+            if (current_leader <= arr[i]) {
+                current_leader = arr[i];
+                array.add(current_leader);
+            }
+        }
+        Collections.reverse(array);
+
+        return (array);
+    }
+}
 
 class Solution {
-    long birthdayGift(int[] A, int[] B, int N) {
-        // Your code here
-        long sum = 0;
-        Arrays.sort(A);
-        Arrays.sort(B);
-        for (int i = 0; i < N; i++) {
-            sum += Math.abs(A[i] - B[i]);
-        }
-        return sum;
-    }
-}
-
-class Main {
-    static int countMakers(int N, int a, int b, int c, int d) {
-        if (a == N || b == N || c == N || d == N)
-            return 1;
-        if (a * b == N || a * c == N || a * d == N || b * c == N || b * d == N || c * d == N)
-            return 2;
-        if (a * b * c == N || a * b * d == N || a * c * d == N || b * c * d == N)
-            return 3;
-        return 4;
-    }
-
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int N = input.nextInt();
-        int a = input.nextInt();
-        int b = input.nextInt();
-        int c = input.nextInt();
-        int d = input.nextInt();
-        System.out.print(countMakers(N, a, b, c, d));
-    }
-}
-
-class GFG {
-
-    // method - returns true if the input is a happy
-    // number else returns false
-    static boolean isHappynumber(int n) {
-        if (n == 1 || n == 7)
-            return true;
-        int sum = n, x = n;
-
-        // this loop executes till the sum of square of
-        // digits obtained is not a single digit number
-        while (sum > 9) {
-            sum = 0;
-
-            // this loop finds the sum of square of digits
-            while (x > 0) {
-                int d = x % 10;
-                sum += d * d;
-                x /= 10;
+    static int findMinLength(String strings[], int n) {
+        int min = strings[0].length();
+        for (int i = 1; i < n; i++) {
+            if (strings[i].length() < min) {
+                min = strings[i].length();
             }
-            if (sum == 1)
-                return true;
-            x = sum;
         }
-        if (sum == 7)
-            return true;
-        return false;
+        return (min);
     }
 
-    // Driver code
-    public static void main(String[] args) {
-        int n = 13;
-        if (isHappynumber(n))
-            System.out.println(n +
-                    " is a Happy number");
-        else
-            System.out.println(n +
-                    " is not a Happy number");
-    }
+    public static String longestCommonStarting(String[] strings, int n) {
+        // Write your code here
+        int minlen = findMinLength(strings, n);
+        String result = "";
+        char current;
 
+        for (int i = 0; i < minlen; i++) {
+            current = strings[0].charAt(i);
+            for (int j = 1; j < n; j++) {
+                if (strings[j].charAt(i) != current) {
+                    if (result.length() == 0)
+                        result = "-1";
+                    return result;
+                }
+            }
+            result += (current);
+        }
+        if (result.length() == 0)
+            result = "-1";
+        return (result);
+    }
 }
 
-    static boolean isHappyNumber(int n) {
-        HashSet<Integer> st = new HashSet<>();
-        while (true) {
-            n = numSquareSum(n);
-            if (n == 1)
-                return true;
-            if (st.contains(n))
-                return false;
-            st.add(n);
+class Solution {
+
+    public static void reverse(char[] a) {
+        int i, n = a.length;
+        char t;
+        for (i = 0; i < n / 2; i++) {
+            t = a[i];
+            a[i] = a[n - i - 1];
+            a[n - i - 1] = t;
         }
     }
 
-static int numSquareSum(int n)
-{
-    int squareSum = 0;
-    while (n!= 0)
-    {
-        squareSum += (n % 10) * (n % 10);
-        n /= 10;
+    public String AliceLikesItEven(String S) {
+        int MAX = 10;
+        String maxEven = "";
+        int len = S.length();
+        int[] freq = new int[MAX];
+        int i, minEvenDigit = MAX;
+        for (i = 0; i < len; i++) {
+            int digit = S.charAt(i) - '0';
+            freq[digit]++;
+            if (digit % 2 == 0)
+                minEvenDigit = Math.min(digit, minEvenDigit);
+
+        }
+        if (minEvenDigit == MAX) {
+            char tempArray[] = S.toCharArray();
+            Arrays.sort(tempArray);
+            reverse(tempArray);
+            return new String(tempArray);
+        }
+        freq[minEvenDigit]--;
+        i = MAX - 1;
+        while (i >= 0) {
+            if (freq[i] > 0) {
+                maxEven = maxEven + i;
+                freq[i]--;
+            } else
+                i--;
+        }
+        maxEven = maxEven + minEvenDigit;
+        return maxEven;
     }
-    return squareSum;
 }
